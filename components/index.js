c58054597021e3c82d19f503a22f62a0adcd8f77
rohitverse <rohitkr23072001@gmail.com>
2023-03-05 16:54:18 +0530

    first commit

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Button from './Button';
+import Banner from './Banner';
+import CreatorCard from './CreatorCard';
+import NFTCard from './NFTCard';
+
+export { Navbar, Banner, Footer, Button, CreatorCard, NFTCard };
---
+++ pages/nft-details.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NFTDetails = () => (
+  <div>
+    <h1>NFTDetails</h1>
+  </div>
+);
+
+export default NFTDetails;
